b2946ad232f30807b382c45016676c4ad4b0ac08
Илья <marchenko.ilya99@gmail.com>
2023-08-13 22:23:44 +0200

    config: Update app.module and Configuration Files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GenerationModule } from 'modules/generator-data/generator.module';
+import { StaffMember } from 'modules/staff-member/staffMember.entity';
+import { StaffMemberModule } from 'modules/staff-member/staffMember.module';
+import { CalculateModule } from 'modules/сalculator/calculate.module';
 
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'D:/DataBase/Test2.db',
+      entities: [StaffMember],
+      synchronize: true,
+    }),
+    StaffMemberModule,
+    GenerationModule,
+    CalculateModule,
+  ],
+})
 export class AppModule {}
