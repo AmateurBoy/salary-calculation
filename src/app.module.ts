4874506ab7cca4ff1966144f39a061a403ccc60c
Илья <marchenko.ilya99@gmail.com>
2023-08-14 18:28:51 +0200

    refactor: add .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { GenerationModule } from 'modules/generator-data/generator.module';
 import { StaffMember } from 'modules/staff-member/staffMember.entity';
 import { StaffMemberModule } from 'modules/staff-member/staffMember.module';
 import { CalculateModule } from 'modules/сalculator/calculate.module';
+import { join } from 'path';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'D:/DataBase/Test2.db',
+      database: join(__dirname, '..', process.env.DATABASE_PATH),
       entities: [StaffMember],
       synchronize: true,
     }),
