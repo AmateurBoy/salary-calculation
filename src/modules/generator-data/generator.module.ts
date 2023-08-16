54aa8a49b17267828904ff83e4fbee46f90f983e
Илья <marchenko.ilya99@gmail.com>
2023-08-16 14:55:30 +0200

    Refactor: use global module

--- src/modules/generator-data/generator.module.ts
+++ src/modules/generator-data/generator.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { GeneratorController } from './generator.controller';
 import { GenerationService } from './generation.service';
-import { StaffMember } from 'modules/staff-member/staffMember.entity';
-import { StaffMemberService } from 'modules/staff-member/staffMember.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StaffMember])],
   controllers: [GeneratorController],
-  providers: [GenerationService, StaffMemberService],
+  providers: [GenerationService],
   exports: [GenerationService],
 })
 export class GenerationModule {}
--- src/modules/staff-member/staffMember.module.ts
+++ src/modules/staff-member/staffMember.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StaffMember } from './staffMember.entity';
 import { StaffMemberController } from './staffMember.controlle';
 import { StaffMemberService } from './staffMember.service';
 
+@Global()
 @Module({
   imports: [TypeOrmModule.forFeature([StaffMember])],
   controllers: [StaffMemberController],
